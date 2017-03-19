cc814a4d800fc0a1bb1c4915a91e1cb62e3cbf74
Cornelia Davis <cornelia@corneliadavis.com>
2017-03-18 21:10:20 -0700

    Anti-pattern: Stateful service that stores valid tokens locally

--- cloudnative-helloworld/src/main/java/com/corneliadavis/cloudnative/CloudnativeHelloworldApplication.java
+++ cloudnative-helloworld/src/main/java/com/corneliadavis/cloudnative/CloudnativeHelloworldApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
 public class CloudnativeHelloworldApplication {
+
+	public static Map<String, String> validTokens = new HashMap<String, String>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudnativeHelloworldApplication.class, args);
