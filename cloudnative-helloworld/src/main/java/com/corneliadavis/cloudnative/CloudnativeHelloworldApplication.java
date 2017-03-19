dba4a57aede4e301ba01e847e8a19d86948b1cf5
Cornelia Davis <cornelia@corneliadavis.com>
2017-03-18 18:34:50 -0700

    Simple greeting controller. Configured via environment variable.

--- cloudnative-helloworld/src/main/java/com/corneliadavis/cloudnative/CloudnativeHelloworldApplication.java
+++ cloudnative-helloworld/src/main/java/com/corneliadavis/cloudnative/CloudnativeHelloworldApplication.java
@@ -2,7 +2,11 @@ package com.corneliadavis.cloudnative;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 public class CloudnativeHelloworldApplication {
 
---
+++ cloudnative-helloworld/src/main/java/com/corneliadavis/cloudnative/Greeting.java
@@ -0,0 +1,18 @@
+package com.corneliadavis.cloudnative;
+
+/**
+ * Created by cdavis on 12/28/16.
+ */
+public class Greeting {
+
+    private String greeting;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+}
